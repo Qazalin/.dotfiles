9cfdd9b34376078f26a409dbd1371cccebaf197c
qazal <ghazal.nzhrzr@gmail.com>
2022-08-15 13:38:18 +0100

    feat: tokyonight

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -1,5 +1,5 @@
---vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_transparent = true
--- vim.opt.background = "dark"
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = true
+vim.opt.background = "dark"
 
--- vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme tokyonight")

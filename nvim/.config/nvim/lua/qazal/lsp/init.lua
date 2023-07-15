a1ea149c260ab54ffbb53c4e204e91de06790696
qazal <qazal.saamani@gmail.com>
2023-07-15 21:03:32 +0300

    actually use neodev

--- nvim/.config/nvim/lua/qazal/lsp/init.lua
+++ nvim/.config/nvim/lua/qazal/lsp/init.lua
@@ -1,8 +1,10 @@
+require("qazal.lsp.neodev")
+
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
-require "qazal.lsp.mason"
-require "qazal.lsp.handlers"
-require "qazal.lsp.null-ls"
+require("qazal.lsp.mason")
+require("qazal.lsp.handlers")
+require("qazal.lsp.null-ls")

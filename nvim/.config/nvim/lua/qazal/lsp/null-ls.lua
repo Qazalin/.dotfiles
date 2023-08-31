ecfbe21b652c42a738a7f8794d0aead04cfa74bd
qazal <qazal.software@gmail.com>
2023-08-31 12:37:44 +0300

    don't allow whitespaces in yapf

--- nvim/.config/nvim/lua/qazal/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/qazal/lsp/null-ls.lua
@@ -1,21 +1,24 @@
 local null_ls = require("null-ls")
 null_ls.setup({
-    debug = false,
-    sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.sqlfmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.yapf.with({
-            extra_args = {
-                -- https://github.com/google/yapf#knobs
-                "--style",
-                "{ \
+	debug = false,
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.sqlfmt,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.yapf.with({
+			extra_args = {
+				-- https://github.com/google/yapf#knobs
+				"--style",
+				"{ \
                     based_on_style: google, \
                     column_limit: 120, \
                     indent_width: 2, \
+                    join_multiple_lines: true, \
+                    blank_lines_between_top_level_imports_and_variables: false, \
+                    blank_line_before_nested_class_or_def: false, \
                 }",
-            },
-        }),
-        null_ls.builtins.diagnostics.mypy, -- just dont use % with new python files this is annoying
-    },
+			},
+		}),
+		null_ls.builtins.diagnostics.mypy, -- just dont use % with new python files this is annoying
+	},
 })

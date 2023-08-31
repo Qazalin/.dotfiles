c69f1c99125084895835c665ea9e249b7784abc5
qazal <qazal.software@gmail.com>
2023-08-31 12:19:05 +0300

    i ended up just using differnet tools

--- nvim/.config/nvim/lua/qazal/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/qazal/lsp/null-ls.lua
@@ -1,20 +1,21 @@
 local null_ls = require("null-ls")
 null_ls.setup({
-	debug = false,
-	sources = {
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.sqlfmt,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.yapf.with({
-			extra_args = {
-				-- https://github.com/google/yapf#knobs
-				"--style",
-				"{ \
+    debug = false,
+    sources = {
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.sqlfmt,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.yapf.with({
+            extra_args = {
+                -- https://github.com/google/yapf#knobs
+                "--style",
+                "{ \
                     based_on_style: google, \
                     column_limit: 120, \
                     indent_width: 2, \
                 }",
-			},
-		}),
-	},
+            },
+        }),
+        null_ls.builtins.diagnostics.mypy, -- just dont use % with new python files this is annoying
+    },
 })

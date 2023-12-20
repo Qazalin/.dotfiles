374943cc0807162e4550a744a6a67395f5131f69
qazal <qazal.software@gmail.com>
2023-12-20 14:47:34 +0200

    devicons is ok

--- nvim/.config/nvim/lua/qazal/packer.lua
+++ nvim/.config/nvim/lua/qazal/packer.lua
@@ -28,6 +28,7 @@ return require("packer").startup(function(use)
 
 	use("hrsh7th/nvim-cmp")
 	use("hrsh7th/cmp-buffer")
+	use("nvim-tree/nvim-web-devicons")
 	use("hrsh7th/cmp-path")
 	use("hrsh7th/cmp-cmdline")
 	use("hrsh7th/cmp-nvim-lsp")

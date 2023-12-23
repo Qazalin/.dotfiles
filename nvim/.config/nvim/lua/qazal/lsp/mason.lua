723e06ad40c8a9a5520b775b4ea29312cfe1ed77
qazal <qazal.software@gmail.com>
2023-12-23 21:26:39 +0200

    rust_analyzer

--- nvim/.config/nvim/lua/qazal/lsp/mason.lua
+++ nvim/.config/nvim/lua/qazal/lsp/mason.lua
@@ -1,32 +1,32 @@
 local servers = {
-    "lua_ls",
-    "pyright",
-    "tsserver",
+	"lua_ls",
+	"pyright",
+	"tsserver",
+	"rust_analyzer",
+	"stylua",
 }
 
-require("mason").setup({
-    log_level = vim.log.levels.INFO,
-})
+require("mason").setup({ log_level = vim.log.levels.INFO })
 require("mason-lspconfig").setup({})
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-    return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("qazal.lsp.handlers").on_attach,
-    }
+	opts = {
+		on_attach = require("qazal.lsp.handlers").on_attach,
+	}
 
-    server = vim.split(server, "@")[1]
+	server = vim.split(server, "@")[1]
 
-    local require_ok, conf_opts = pcall(require, "qazal.lsp.settings." .. server)
-    if require_ok then
-        opts = vim.tbl_deep_extend("force", conf_opts, opts)
-    end
+	local require_ok, conf_opts = pcall(require, "qazal.lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end

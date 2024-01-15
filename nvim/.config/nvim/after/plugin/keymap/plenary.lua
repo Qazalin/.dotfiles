b374e4b03c32b26c93bb0560d2f7ed89629445ab
qazal <qazal.software@gmail.com>
2024-01-16 00:44:12 +0200

    generic nvim test cmd

--- nvim/.config/nvim/after/plugin/keymap/plenary.lua
+++ nvim/.config/nvim/after/plugin/keymap/plenary.lua
@@ -1,4 +1,17 @@
 local Remap = require("qazal.keymap")
 local nnoremap = Remap.nnoremap
 
-nnoremap("<leader>t", "<Plug>PlenaryTestFile")
+nnoremap("<leader>t", function()
+	local ext = vim.fn.expand("%:e")
+
+	if ext == "lua" then
+		return "<Plug>PlenaryTestFile"
+	elseif ext == "rs" then
+		vim.cmd(":!cargo test")
+	elseif ext == "ts" then
+		local fp = vim.fn.expand("%:p")
+		vim.cmd(":!pnpm vitest run " .. fp)
+	else
+		print("No test command for ." .. ext .. " files")
+	end
+end, { expr = true })

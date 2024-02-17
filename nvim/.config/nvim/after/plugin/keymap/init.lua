87b60a5b5c8eb0079238a3e815bf79cac0e0efc4
qazal <qazal.software@gmail.com>
2024-02-17 11:42:35 +0000

    the greatest remap of all time

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -26,9 +26,23 @@ xnoremap("<leader>p", '"_dP')
 nnoremap("<leader>v", "vt$")
 
 -- second greatest remap ever
-nnoremap("<leader>y", '"+y')
-vnoremap("<leader>y", '"+y')
-nnoremap("<leader>Y", 'gg"+yG')
+
+-- nnoremap("<leader>y", '"+y')
+-- vnoremap("<leader>y", '"+y')
+-- nnoremap("<leader>Y", 'gg"+yG')
+function write_visual_select_to_file()
+    local start_pos = vim.fn.getpos("'<")
+    local end_pos = vim.fn.getpos("'>")
+    local lines = vim.fn.getline(start_pos[2], end_pos[2])
+    if #lines == 0 then return end
+    -- adjust the first line to start from the actual selection start
+    lines[1] = string.sub(lines[1], start_pos[3], -1)
+    -- adjust the last line to end at the actual selection end
+    lines[#lines] = string.sub(lines[#lines], 1, end_pos[3] - start_pos[3])
+    local file_path = vim.fn.expand("~/custom-clipboard")
+    vim.fn.writefile(lines, file_path)
+end
+vim.api.nvim_set_keymap('v', '<leader>y', ':lua write_visual_select_to_file()<CR>', {noremap = true, silent = true})
 
 nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 nnoremap("<leader><leader>w", "<cmd>w<CR><cmd>source %<CR>", { silent = true })
